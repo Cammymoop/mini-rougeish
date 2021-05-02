f10d94c7d17a5e13d83bc3a9640dc29d3cbd6691
Charlie Burnham <felixtheslayer@gmail.com>
2021-05-02 18:19:18 -0500

    more sprite edits, add big cubeo

--- src/generation.py
+++ src/generation.py
@@ -159,15 +159,21 @@ def room_furnisher(world, tile_map, x, y, w, h):
         if stuff_so_far >= max_things:
             return
 
+    enemy_sets = [
+        ['goon'],
+        ['goon', 'goon', 'goon', 'cubeo'],
+        ['cubeo', 'cubeo', 'big_cubeo']
+    ];
+    # Random chance of eyepod in room with 1 enemy
+    if include_enemies == 1 and random.randint(1, 9) < 2:
+        enemy_sets = [['eyepod']]
+    enemy_set = random.choice(enemy_sets)
     for i in range(include_enemies):
         spot = random.choice(unused_spots)
         unused_spots.remove(spot)
         spot_x, spot_y = spot
 
-        enemy_type = random.choice(['goon', 'goon', 'goon', 'cubeo'])
-        if include_enemies == 1 and random.randint(1, 9) < 2:
-            enemy_type = 'eyepod'
-
+        enemy_type = random.choice(enemy_set)
         world.add_entity_at(spot_x, spot_y, False, 'creature', enemy_type)
 
         stuff_so_far += 1
